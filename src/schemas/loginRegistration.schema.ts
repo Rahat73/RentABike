8955d7eb553075ad62340606ec417e43ede39d09
Rahat73 <rahat.ashik.18@gmail.com>
2024-09-02 18:23:20 +0600

    validation update

--- src/schemas/loginRegistration.schema.ts
+++ src/schemas/loginRegistration.schema.ts
@@ -16,12 +16,16 @@ export const registrationSchema = z.object({
   email: z
     .string({ required_error: "email is required" })
     .email({ message: "Invalid email address" }),
-  password: z.string({
-    required_error: "Password is required",
-  }),
-  phone: z.string({
-    required_error: "Phone number is required",
-  }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(6, "Password must be at least 6 characters long"),
+  phone: z
+    .string({
+      required_error: "Phone number is required",
+    })
+    .min(10, "Invalid phone number (minimum length should be 10)"),
   address: z.string({
     required_error: "Address is required",
   }),
